a8dbbf3a380d0f49f681d94636b85a90ac624ed9
mermehr <edward.forsyth@gmail.com>
2023-06-19 15:37:35 -0500

    update

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1.0f;
+    [SerializeField] float maxSpawnDelay = 5.0f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
